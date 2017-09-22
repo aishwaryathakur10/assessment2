5de968d31c7a8406213c8bffb339620001c6ee33
Aishwarya Thakur <aishwarya.thakur1@gmail.com>
2017-09-22 19:56:51 +0530

    commit

---
+++ TravelBooking/TravelBooking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TravelBooking.Startup))]
+namespace TravelBooking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
